c947ed1d794e993f717f0af86bf73e1c8f4f2f88
baekalfen <mynddal@me.com>
2014-02-20 17:18:29 +0100

    initial commit from local git

---
+++ Yamaha-AV-Controller/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Yamaha-AV-Controller
+//
+//  Created by Mads Ynddal on 20/02/14.
+//  Copyright (c) 2014 Mads Y. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
